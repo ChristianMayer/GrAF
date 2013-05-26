8d037b69c3c46e78e388f2dfcde00def80235293
mayerch <mayerch@b3ce15a5-05c3-4bc8-b6eb-98bdc3a999cb>
2013-05-26 20:57:52 +0000

    New try at the editor. Early work in progress. Should have much better performance.

---
+++ GrAF/trunk/GrAFd/editor2/lib/Vec2D.js
@@ -0,0 +1,228 @@
+/**
+ * Vec2D.js (c) 2013 by Christian Mayer [CometVisu at ChristianMayer dot de]
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ * 
+ * @module Vec2D
+ * @title  GrAF logic engine: graphical logic editor
+ */
+
+/**
+ * Vec2D - a JS library for 2D vector operations.
+ * Design decision:
+ *   All operations modify the source!
+ *   All operations are chainable.
+ * 
+ * Exceptions are marked explicitly.
+ * If the source mustn't be changes, use the copy() method.
+ */ 
+// create a local context:
+(function( window, undefined ) {
+  "use strict";
+
+  var Vec2D = function( optionalX, optionalY ) {
+    if( !( this instanceof Vec2D ) )
+      throw 'Error, use "new" operator for Vec2D!';
+
+    if( 'object' === typeof optionalX && 
+        undefined !== optionalX.x     &&
+        undefined !== optionalX.y        )
+    {
+      this.x = +optionalX.x;
+      this.y = +optionalX.y;
+    } else {
+      this.x = +optionalX;
+      this.y = +optionalY;
+    }
+  };
+    
+  // fill the prototype public methods of GLE:
+  Vec2D.prototype.toString = function() { return '[object Vec2D]'; };
+  Vec2D.prototype.print    = function() {
+    return this.x + '/' + this.y;
+  };
+  
+  Vec2D.prototype.copy = function() {
+    return new Vec2D( this.x, this.y );
+  };
+  Vec2D.prototype.replace = function( other ) {
+    this.x = +other.x;
+    this.y = +other.y;
+    return this;
+  };
+  Vec2D.prototype.plus = function( other ) {
+    this.x += +other.x;
+    this.y += +other.y;
+    return this;
+  };
+  Vec2D.prototype.minus = function( other ) {
+    this.x -= +other.x;
+    this.y -= +other.y;
+    return this;
+  };
+  /**
+   * coefficient multiplication
+   */
+  Vec2D.prototype.cmul = function( other ) {
+    if( other instanceof Vec2D )
+    {
+      this.x *= +other.x;
+      this.y *= +other.y;
+      return this;
+    } else {// treat as array
+      this.x = this.x * +other[0] | 0;
+      this.y = this.y * +other[1] | 0;
+      return this;
+    }
+  };
+  /**
+   * Scalar product.
+   */
+  Vec2D.prototype.sprod = function( other ) {
+    return this.x * +other.x + this.y * +other.y;
+  };
+  
+  /**
+   * Compare two Vec2D. 
+   * @param {Number} epsilon - if set use that as a tolerance, otherwise exact match.
+   */
+  Vec2D.prototype.equal = function( other, epsilon ) {
+    if( undefined === epsilon )
+      return this.x === other.x && this.y === other.y;
+    
+    var tmp = this.copy().minus( other );
+    return tmp.sprod( tmp ) <= epsilon*epsilon;
+  };
+  
+  /**
+   * Scale vector to passed length.
+   * If length is 1.0 it will be a unit vector.
+   */
+  Vec2D.prototype.toLength = function( length ) {
+    var scale = +length / Math.sqrt( this.sprod( this ) );
+    this.x *= scale;
+    this.y *= scale;
+    return this;
+  };
+  /**
+   * Calculate the normal vector.
+   */
+  Vec2D.prototype.getNormal = function() {
+    return new Vec2D( this.y, -this.x );
+  };
+  /**
+   * Calculate a norm.
+   */
+  Vec2D.prototype.getNorm = function() {
+    return this.sprod( this );
+  };
+  
+  /**
+   * Set qualifier.
+   */
+  Vec2D.prototype.setQualifier = function( key, value ) {
+    this[key] = value;
+    return this;
+  };
+  
+  if( undefined !== window.Vec2D )
+  {
+    throw 'Error: Object named "Vec2D" already exists!';
+  } else {
+    window.Vec2D = Vec2D;
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////
+  // Line                                                                     //
+  //////////////////////////////////////////////////////////////////////////////
+  
+  var Line = function( start, end ) {
+    if( !( this instanceof Line ) )
+      throw 'Error, use "new" operator for Line!';
+
+    if( !( start instanceof Vec2D ) || !( end instanceof Vec2D ) )
+      throw 'Error, parameters has to be of type Vec2D!';
+    
+    this.start = start;
+    this.end   = end;
+  };
+    
+  // fill the prototype public methods of GLE:
+  Line.prototype.toString = function() { return '[object Line]'; };
+  
+  /**
+   * Calculate the intersection point of two lines.
+   * When the @retrurn value num1 or num2 is divided by det you have the
+   * intersection point in local coordinates.
+   */
+  Line.prototype.calcIntersection = function( otherLine )
+  {
+    var dir1 = this.end.copy().minus( this.start ),
+        dir2 = otherLine.end.copy().minus( otherLine.start ),
+        diff = this.start.copy().minus( otherLine.start ),
+        num1 = dir2.x * diff.y - dir2.y * diff.x,
+        num2 = dir1.x * diff.y - dir1.y * diff.x,
+        det  = dir2.y * dir1.x - dir2.x * dir1.y;
+    return [ num1, num2, det ];
+  };
+  
+  /**
+   * Return true if both line segments do intersect.
+   */
+  Line.prototype.checkSegmentIntersection = function( otherLine )
+  {
+    var intersection = this.calcIntersection( otherLine ),
+        num1 = intersection[0],
+        num2 = intersection[1],
+        det  = intersection[2];
+    
+    if( 0 === det ) return false; // lines parallel, probably identical...
+    
+    var t1 = num1 / det,
+        t2 = num2 / det;
+    console.log( num1, num2, det, t1, t2 );    
+    return (t1 >= 0) && (t1 <= 1) && (t2 >= 0) && (t2 <= 1); 
+  };
+  
+  /**
+   * Return true if the point is closer to the segment than the epsilon
+   */
+  Line.prototype.checkPointProximity = function( point, epsilon )
+  {
+    var ps = point.copy().minus( this.start ),
+        es = this.end.copy().minus( this.start ),
+        num = ps.sprod( es ),
+        det = es.sprod( es );
+      
+    if( (num < 0) || (num > det) )
+      return false; // point outside of segment
+      
+    var s = ( ps.x*es.y - ps.y*es.x ) / det;
+    
+    return Math.abs( s ) * Math.sqrt( det ) < epsilon;
+    /*    
+     (Cx-Ax)(Bx-Ax) + (Cy-Ay)(By-Ay)
+        r = -------------------------------
+                          L^2
+    */
+  }
+
+  if( undefined !== window.Line )
+  {
+    throw 'Error: Object named "Line" already exists!';
+  } else {
+    window.Line = Line;
+  }
+})( typeof window === 'undefined' ? self : window );
---
+++ GrAF/trunk/GrAFd/editor2/lib/gle.block.js
@@ -0,0 +1,437 @@
+/**
+ * gle.block.js (c) 2013 by Christian Mayer [CometVisu at ChristianMayer dot de]
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ * 
+ * @module GLE.Block
+ * @title  GrAF logic engine: graphical logic editor
+ */
+ 
+// create a local context:
+(function( window, undefined ) {
+  "use strict";
+  
+  // Constructor
+  var block = function( thisGLE ){
+    if( !( this instanceof block ) )
+      throw 'Error, use "new" operator for Block!';
+    
+    // private:
+    var self     = this,
+        pos      = new Vec2D( 0, 0 ),  // geometry
+        size     = new Vec2D( 100, 100 ),
+        minWidth = 5,
+        minHeight = 5,
+        color    = '#000000',          // how to display
+        fill     = '#ffffff',
+        name     = '',
+        inPorts  = [], 
+        outPorts = [],
+        handlers = [],
+        getInPortPos = function( index ){
+          var centerY = (size.y * (index+0.5) / inPorts.length) | 0;
+          return pos.copy().plus( new Vec2D( 0     , centerY ) );
+        },
+        getOutPortPos = function( index ){
+          var centerY = (size.y * (index+0.5) / outPorts.length) | 0;
+          return pos.copy().plus( new Vec2D( size.x, centerY ) );
+        };
+    
+    this.setName = function( newName ) {
+      name = newName;
+      thisGLE.invalidateContext();
+      return this;
+    };
+    this.getName = function() {
+      return name;
+    }
+    this.setInPorts = function( list ) {
+      inPorts = list;
+      inPorts.forEach( function( entry, i ) {
+        if( 'string' === typeof entry )
+          inPorts[ i ] = { name: entry };
+      } );
+      minHeight = 5 + Math.max( inPorts.length, outPorts.length ) * 10;
+      thisGLE.invalidateHandlers();
+      return this;
+    };
+    this.setInConnection = function( theConnection, portNumber )
+    {
+      if( 0 > portNumber || inPorts.length <= portNumber )
+        throw 'portNumber ' + portNumber + ' is out of range!';
+      
+      inPorts[ portNumber ].connection = theConnection;
+      return this;
+    };
+    this.setOutPorts = function( list ) {
+      outPorts = list;
+      outPorts.forEach( function( entry, i ) {
+        if( 'string' === typeof entry )
+          outPorts[ i ] = { name: entry };
+      } );
+      minHeight = 5 + Math.max( inPorts.length, outPorts.length ) * 10;
+      thisGLE.invalidateHandlers();
+      return this;
+    };
+    this.setOutConnection = function( theConnection, portNumber )
+    {
+      if( 0 > portNumber || outPorts.length <= portNumber )
+        throw 'portNumber ' + portNumber + ' is out of range!';
+      
+      outPorts[ portNumber ].connection = theConnection;
+      return this;
+    };
+    this.setTopLeft = function( coord ) {
+      pos = coord.copy();
+      thisGLE.invalidateContext();
+      return this;
+    };
+    this.getTopLeft = function() {
+      return pos.copy();
+    };
+    this.setBottomRight = function( coord ) {
+      size = coord.minus( pos );
+      thisGLE.invalidateContext();
+      return this;
+    };
+    this.getSize = function() {
+      return size.copy();
+    };
+    this.getInCoordinates = function( handler ) {
+      if( (5 <= handler) && (handler < 5 + inPorts.length) )
+      {
+        var portNumber = handler - 5,
+            endPos = getInPortPos( portNumber );
+        return [ endPos.copy().minus( new Vec2D( 20, 0 ) ), endPos, portNumber ];
+      }
+    }
+    this.getOutCoordinates = function( handler ) {
+      if( handler >= 5 + inPorts.length )
+      {
+        var portNumber = handler - 5 - inPorts.length,
+            startPos = getOutPortPos( portNumber );
+        return [ startPos, startPos.copy().plus( new Vec2D( 10, 0 ) ), portNumber ];
+      }
+    }
+    this.getHandler = function() {
+      return handlers[0];
+    };
+    
+    /**
+     * Return true if the @parm mousePos doesn't belong to this object
+     */
+    this.checkBadSelection = function( mousePos, index, epsilon, scale )
+    {
+      index = index | 0;
+      switch( index )
+      {
+        case 0:
+          return (pos.x > mousePos.x) || 
+                 (pos.y > mousePos.y) || 
+                 ((pos.x+size.x) < mousePos.x) || 
+                 ((pos.y+size.y) < mousePos.y);
+          
+        case 1:
+          return thisGLE.checkHandlerBadSelection( mousePos, pos );
+          
+        case 2:
+          return thisGLE.checkHandlerBadSelection( mousePos, pos.copy().plus( size.copy().cmul([1,0]) ) );
+          
+        case 3:
+          return thisGLE.checkHandlerBadSelection( mousePos, pos.copy().plus( size.copy().cmul([0,1]) ) );
+          
+        case 4:
+          return thisGLE.checkHandlerBadSelection( mousePos, pos.copy().plus( size ) );
+      }
+      
+      if( index < 5 + inPorts.length )
+      {
+        return thisGLE.checkHandlerBadSelection( mousePos, getInPortPos( index - 5 ) );
+      }
+      
+      return thisGLE.checkHandlerBadSelection( mousePos, getOutPortPos( index - 5 - inPorts.length ) );
+    };
+    
+    this.prepareUpdate = function( handler, index, mousePos, ctrlKey, shiftKey )
+    {
+      console.log( 'Block prepareUpdate', index, handler, ctrlKey );
+      if( 0 === handler )
+      {
+        if( ctrlKey )
+        {
+          console.log( 'Duplicate Block' );
+          index = thisGLE.addBlock()
+                  .setTopLeft( pos )
+                  .setBottomRight( pos.copy().plus( size ) )
+                  .setName( thisGLE.getNextName( name ) )
+                  .setInPorts ( inPorts.map ( function(thisPort){ return thisPort.name} ) )
+                  .setOutPorts( outPorts.map( function(thisPort){ return thisPort.name} ) )
+                  .getHandler();
+          thisGLE.invalidateHandlers();
+        }
+      } else if( handler < 5 )
+      {
+      } else if( handler < 5 + inPorts.length )
+      {
+        var thisIndex = handler - 5;
+      } else {
+        var thisIndex = handler - 5 - inPorts.length,
+            coords    = this.getOutCoordinates( handler );
+        outPorts[ thisIndex ].connection = thisGLE
+          .addConnection( { start: { block: this, portNumber: thisIndex }, name: name + '_Out#' + thisIndex } )
+          //.insertWaypoint( getOutPortPos( thisIndex ).setQualifier( 'protected', true ) )
+          //.insertWaypoint( getOutPortPos( thisIndex ).plus( new Vec2D( 10, 0 ) ).setQualifier( 'protected', true ) );
+          .insertWaypoint( coords[0].setQualifier( 'protected', true ) )
+          .insertWaypoint( coords[1].setQualifier( 'protected', true ) );
+          //.insertWaypoint( getOutPortPos( thisIndex ).setQualifier( 'protected', false ) );
+        return outPorts[ thisIndex ].connection.prepareUpdate( 1 );
+        //return outPorts[ thisIndex ].connection.waypoints[1].handler;
+      }
+      
+      return index;
+    };
+    
+    /**
+     * Update the position of the index.
+     */
+    this.update = function( index, newPos, deltaPos, shortDeltaPos )
+    {
+      index = index | 0;
+      console.log( 'block update',  index, newPos, deltaPos, shortDeltaPos );
+      switch( index )
+      {
+        case 0: // main
+          pos.plus( shortDeltaPos );
+          break;
+          
+        case 1: // top left
+          size.plus( pos ).minus( newPos );
+          pos  = newPos;
+          break;
+          
+        case 2: // top right
+          size.x  = newPos.x - pos.x;
+          size.y -= newPos.y - pos.y;
+          pos.y   = newPos.y;
+          break;
+          
+        case 3: // bottom left
+          size.x -= newPos.x - pos.x;
+          pos.x   = newPos.x;
+          size.y  = newPos.y - pos.y;
+          
+          break;
+          
+        case 4: // bottom right
+          size = newPos.minus( pos );
+          break;
+          
+        default:
+          return;
+          if( index < 5 + inPorts.length )
+          {
+            var thisIndex = index - 5;
+            console.log( 'upd add in', index, thisIndex, this );
+            inPorts[ thisIndex ].connection = thisGLE
+              .addConnection()
+              .insertWaypoint( getInPortPos( thisIndex ).setQualifier( 'protected', true ) )
+              .insertWaypoint( getInPortPos( thisIndex ).plus( new Vec2D( 15, 0 ) ).setQualifier( 'protected', true ) );
+          } else {
+            /*
+            var thisIndex = index - 5 - inPorts.length;
+            console.log( 'upd add out', index, thisIndex, this );
+            outPorts[ thisIndex ].connection = thisGLE
+              .addConnection( { start: { block: this, portNumber: thisIndex }, name: name + '_Out#' + thisIndex } )
+              .insertWaypoint( getOutPortPos( thisIndex ).setQualifier( 'protected', true ) )
+              .insertWaypoint( getOutPortPos( thisIndex ).plus( new Vec2D( 15, 0 ) ).setQualifier( 'protected', true ) )
+              .insertWaypoint( getOutPortPos( thisIndex ).plus( new Vec2D( 15, 0 ) ).setQualifier( 'protected', false ) );
+*/
+          }
+      };
+      size.x = Math.max( size.x, minWidth  );
+      size.y = Math.max( size.y, minHeight );
+      
+      inPorts.forEach( function moveInPortConnection_PROFILENAME( thisPort, i ) {
+        console.log( thisPort, i, undefined !== thisPort.connection );
+        if( undefined !== thisPort.connection )
+        {
+          var coords = self.getInCoordinates( i + 5 );
+          var l = thisPort.connection.waypoints.length;
+          var res = thisPort.connection.moveWaypoint( [ l-2, l-1 ], [coords[0], coords[1]], true );
+          /*
+          console.log( coords );
+          var l = thisPort.connection.waypoints.length;
+          //thisPort.connection.waypoints[ l - 2 ].replace( coords[0] );
+          thisPort.connection.waypoints[ l - 1 ].protected = false;
+          thisPort.connection.moveWaypoint( l - 2, coords[0].copy(), true );
+          l = thisPort.connection.waypoints.length;
+          thisPort.connection.waypoints[ l - 1 ].replace( coords[1] );
+          thisPort.connection.waypoints[ l - 1 ].protected = true;
+          */
+        }
+      } );
+      outPorts.forEach( function moveOutPortConnection_PROFILENAME( thisPort, i ) {
+        console.log( thisPort, i, undefined !== thisPort.connection );
+        if( undefined !== thisPort.connection )
+        {
+          var coords = self.getOutCoordinates( i + 5 + inPorts.length );
+          var res = thisPort.connection.moveWaypoint( [0,1], [coords[0], coords[1]], true );
+          /*
+          var l = thisPort.connection.waypoints.length;
+          thisPort.connection.waypoints[ 0 ].protected = false;
+          //thisPort.connection.waypoints[ 1 ] = coords[1];
+          var res = thisPort.connection.moveWaypoint( 1, coords[1].copy(), true );
+          thisPort.connection.waypoints[ 0 ].replace( coords[0] );
+          thisPort.connection.waypoints[ 0 ].protected = true;
+          
+          console.log( coords, res, handlers );
+          */
+        }
+      } );
+      return handlers[ index ]; // return handler again as it might have been renumbered by the connection move
+    }
+    
+    this.finishUpdate = function( index )
+    {
+      console.log( 'Block finishUpdate', index );
+    }
+    
+    /**
+     * Delete itself, i.e. cancel references to it in the connections.
+     */
+    this.delete = function()
+    {
+      inPorts.forEach( function( thisPort ) {
+        if( undefined !== thisPort.connection )
+          thisPort.connection.end = undefined;
+      } );
+      outPorts.forEach( function( thisPort ) {
+        if( undefined !== thisPort.connection )
+          thisPort.connection.start = undefined;
+      } );
+      name = '### Deleted Block! ###'; // Help debuging the GC
+    };
+    
+    /**
+     * Get all the elements that should be considers active when this element
+     * is active
+     */
+    this.getDerivedActive = function()
+    {
+      var retVal = [];
+      inPorts.forEach( function( thisPort ) {
+        if( undefined !== thisPort.connection )
+          retVal.push( thisPort.connection );
+      } );
+      outPorts.forEach( function( thisPort ) {
+        if( undefined !== thisPort.connection )
+          retVal.push( thisPort.connection );
+      } );
+      return retVal;
+    }
+    
+    /**
+     * Draw itself on the canvas @param context and it's shape on the
+     * @param index context.
+     */
+    this.draw = function( context, index, focus ) {
+      // draw shape to index map
+      thisGLE.prepareHandlerDrawing( handlers[ 0 ] );
+      index.fillRect( pos.x, pos.y, size.x, size.y );
+      
+      thisGLE.drawHandler( pos                                       , handlers[ 1 ], focus );
+      thisGLE.drawHandler( pos.copy().plus( size.copy().cmul([1,0]) ), handlers[ 2 ], focus );
+      thisGLE.drawHandler( pos.copy().plus( size.copy().cmul([0,1]) ), handlers[ 3 ], focus );
+      thisGLE.drawHandler( pos.copy().plus( size )                   , handlers[ 4 ], focus );
+      
+      // draw block itself
+      context.save(); // make sure to leave the context alone
+      context.colorStyle = color;
+      context.fillStyle  = fill;
+      context.fillRect( pos.x, pos.y, size.x, size.y );
+      context.strokeRect( pos.x, pos.y, size.x, size.y );
+      
+      context.fillStyle = '#000000';
+      context.textAlign = 'center';
+      context.textBaseline = 'top';
+      
+      context.fillText( name, pos.x + size.x / 2, pos.y + size.y + 2 );
+      
+      context.textBaseline = 'middle';
+      context.textAlign = 'left';
+      var startIndex = 5;
+      inPorts.forEach( function( thisPort, index ){
+        var centerY = (size.y * (index+0.5) / inPorts.length) | 0;
+        context.fillText( thisPort.name, pos.x + 5, pos.y + centerY );
+        if( undefined === thisPort.connection ) 
+        {
+          context.beginPath();
+          context.moveTo( pos.x - 5, pos.y - 5 + centerY );
+          context.lineTo( pos.x    , pos.y     + centerY );
+          context.lineTo( pos.x - 5, pos.y + 5 + centerY );
+          context.stroke(); 
+          thisGLE.drawHandler( getInPortPos( index ), handlers[ startIndex + index ], focus );
+        } else {
+        }
+      });
+      context.textAlign = 'right';
+      startIndex += inPorts.length;
+      outPorts.forEach( function( thisPort, index ){
+        var centerY = (size.y * (index+0.5) / outPorts.length) | 0;
+        context.fillText( thisPort.name, pos.x + size.x - 5, pos.y + centerY );
+        if( undefined === thisPort.connection ) 
+        {
+          context.beginPath();
+          context.moveTo( pos.x + size.x    , pos.y - 5 + centerY );
+          context.lineTo( pos.x + size.x + 5, pos.y     + centerY );
+          context.lineTo( pos.x + size.x    , pos.y + 5 + centerY );
+          context.stroke(); 
+          thisGLE.drawHandler( getOutPortPos( index ), handlers[ startIndex + index ], focus );
+        } else {
+        }
+      });
+      context.restore();
+    };
+    
+    /**
+     * Reregister all handlers, e.g. when they got invalid.
+     */
+    this.reregisterHandlers = function(){
+      handlers.length = 0; 
+      // the first 5 handlers are for the element itself and the 4 handlers
+      for( var i = 0, l = 5 + inPorts.length + outPorts.length; i < l; i++ )
+      {
+        handlers.push( thisGLE.registerHandler( this, i ) );
+      }
+    }
+    
+    // constructor
+    this.reregisterHandlers(); // initial registering
+  };
+  
+  
+  block.prototype.toString = function(){
+    return '[object GLE:block]';
+  };
+  
+  // create namespace if necessary
+  if( undefined === window._GLE )
+    window._GLE = {};
+  
+  if( undefined !== window._GLE.block )
+    throw 'Error: "block" already in "_GLE" namespace!';
+  
+  window._GLE.block = block;
+})( window );
